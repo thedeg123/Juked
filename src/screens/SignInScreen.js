a8b52b780198ce9fd826874bcdf84a5522a3606c
david <thedeg123@gmail.com>
2020-01-16 14:34:21 -0700

    cleaned useless  imports

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,7 +1,6 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { View, StyleSheet, Button } from "react-native";
 import { Context as AuthContext } from "../context/AuthContext";
-import { Text, Input } from "react-native-elements";
 import AuthForm from "../components/AuthForm";
 
 const SignInScreen = ({ navigation }) => {
