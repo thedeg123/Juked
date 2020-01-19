3cc7bd2f1fe5e95cd2c179cde889755b2ec8844b
david <thedeg123@gmail.com>
2020-01-19 13:28:01 -0700

    Added  useMusic hook...kinda

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 import useMusic from "../hooks/useMusic";
 
 const HomeScreen = ({ navigation }) => {
-  const { songs } = useMusic();
+  const [state, findAlbums, findArtists, findTracks, searchAPI] = useMusic();
+  useEffect(() => {
+    findArtists("0OdUWJ0sBjDrqHygGUXeCF");
+    console.log(state); //this doesnt print as it should. Is that becuase its printing before the promise is complete? I have no idea!
+  }, []);
   return (
     <View>
       <Text style={styles.headerStyle}>HomeScreen</Text>
@@ -41,5 +45,4 @@ const styles = StyleSheet.create({
     fontSize: 60
   }
 });
-
 export default HomeScreen;
