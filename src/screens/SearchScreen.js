b3d9c7784654affa83d9d0312ac05da999bb04b0
Alex Jensen <ajensen@oberlin.edu>
2020-01-20 17:16:36 -0500

    Added search functionality, no error handling

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -2,20 +2,37 @@ import React, { useState } from "react";
 import { Text, View, StyleSheet, Button } from "react-native";
 import SearchBar from "../components/SearchBar";
 import SearchStyle from "../components/SearchStyle";
+import useMusic from "../hooks/useMusic";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
   const [searchType, setSearchType] = useState("song");
-
+  const {
+    tracks,
+    albums,
+    artists,
+    search,
+    findAlbums,
+    findArtists,
+    findTracks,
+    searchAPI
+  } = useMusic();
   return (
     <View style={styles.container}>
       <SearchBar
         term={term}
         onTermChange={setTerm}
-        onTermSubmit={() => console.log("To do")}
+        onTermSubmit={() => {
+          searchAPI(term, searchType);
+        }}
       />
       <SearchStyle searchType={searchType} setSearchType={setSearchType} />
       <Text>Searching for {searchType}:</Text>
+      <Text>
+        {search
+          ? `${search.artists.items[0].name}: ${search.artists.items[0].id}`
+          : "none"}
+      </Text>
     </View>
   );
 };
