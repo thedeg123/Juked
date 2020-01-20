51d5aa3e376bf88dffbd899f0789b58777e11ca0
Alex Jensen <ajensen@oberlin.edu>
2020-01-20 15:38:48 -0500

    Added buttons for search type

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Button } from "react-native";
 import SearchBar from "../components/SearchBar";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
+  const [searchType, setSearchType] = useState("song");
 
   return (
     <View style={styles.container}>
@@ -12,6 +13,12 @@ const SearchScreen = () => {
         onTermChange={setTerm}
         onTermSubmit={() => console.log("To do")}
       />
+      <View style={styles.searchTypeStyle}>
+        <Button title="Songs" style={styles.buttonStyle} />
+        <Button title="Albums" style={styles.buttonStyle} />
+        <Button title="Artists" style={styles.buttonStyle} />
+        <Button title="Users" style={styles.buttonStyle} />
+      </View>
     </View>
   );
 };
@@ -19,6 +26,17 @@ const SearchScreen = () => {
 const styles = StyleSheet.create({
   container: {
     marginTop: 20
+  },
+  searchTypeStyle: {
+    marginTop: 10,
+    marginHorizontal: 10,
+    flexDirection: "row",
+    borderWidth: 1,
+    borderColor: "#3480eb",
+    justifyContent: "space-between"
+  },
+  buttonStyle: {
+    marginHorizontal: 20
   }
 });
 
