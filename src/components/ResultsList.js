0cd2443d7343dfb0dd79761c0ad25be5d58db698
Alex Jensen <ajensen@oberlin.edu>
2020-01-21 14:50:42 -0500

    Testing clicking on album

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Text, View, StyleSheet, Button } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import SearchPreview from "./SearchPreview";
 
 const ResultsList = ({ term, searchType, search, navigation }) => {
@@ -7,6 +7,7 @@ const ResultsList = ({ term, searchType, search, navigation }) => {
     return (
       <View>
         <SearchPreview
+          //root={search.tracks.items[0]}
           title={search ? `${search.tracks.items[0].name}` : "none"}
           type={searchType}
           music_id={search ? `${search.tracks.items[0].id}` : "none"}
@@ -23,6 +24,7 @@ const ResultsList = ({ term, searchType, search, navigation }) => {
     return (
       <View>
         <SearchPreview
+          //root={search ? `${search.albums.items[0]}` : "none"}
           title={search ? `${search.albums.items[0].name}` : "none"}
           type={searchType}
           music_id={search ? `${search.albums.items[0].id}` : "none"}
@@ -39,6 +41,7 @@ const ResultsList = ({ term, searchType, search, navigation }) => {
     return (
       <View>
         <SearchPreview
+          //root={search ? `${search.artist.items[0]}` : "none"}
           title={search ? `${search.artist.items[0].name}` : "none"}
           type={searchType}
           music_id={search ? `${search.artist.items[0].id}` : "none"}
