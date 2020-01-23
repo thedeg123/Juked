ad5bdc490c0f8dc23f7c15615295e6a8a0d9bea3
sheronW <sheronw1174@gmail.com>
2020-01-23 12:36:17 +0800

    add two-column layout for ArtistScreen

--- src/screens/AlbumScreen.js
+++ src/screens/AlbumScreen.js
@@ -1,10 +1,21 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 
-const AlbumScreen = () => {
+const AlbumScreen = ({ navigation }) => {
+  const music_id = navigation.getParam("music_id");
+  const title = navigation.getParam("title");
   return (
     <View>
       <Text style={styles.headerStyle}>AlbumScreen</Text>
+      <FlatList
+        data={l}
+        keyExtracter={i => i.item.name}
+        renderItem={i => {
+          return <ListPreview result={i.item} />;
+        }}
+        columnWrapperStyle={styles.column}
+        numColumns={2}
+      />
     </View>
   );
 };
--- src/screens/ArtistScreen.js
+++ src/screens/ArtistScreen.js
@@ -1,10 +1,22 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
-const ArtistScreen = () => {
+const ArtistScreen = ({ navigation }) => {
+  const music_id = navigation.getParam("music_id");
+  const title = navigation.getParam("title");
+  const results;
   return (
     <View>
       <Text style={styles.headerStyle}>ArtistScreen</Text>
+      <FlatList
+        data={results}
+        keyExtracter={i => i.item.name}
+        renderItem={i => {
+          return <ListPreview result={i.item} />;
+        }}
+        columnWrapperStyle={styles.column}
+        numColumns={2}
+      />
     </View>
   );
 };
@@ -12,7 +24,8 @@ const ArtistScreen = () => {
 const styles = StyleSheet.create({
   headerStyle: {
     fontSize: 60
-  }
+  },
+  column: { flexShrink: 1, width: "50%" }
 });
 
 export default ArtistScreen;
