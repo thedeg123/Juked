a65721dec8c44d60dc63ef7dce857463d23c751a
david <thedeg123@gmail.com>
2020-01-23 12:58:53 -0700

    began homescreen

---
+++ src/components/Container.js
@@ -0,0 +1,13 @@
+//Provides margin on components
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Container = ({ children }) => {
+  return <View style={styles.containerStyle}>{children}</View>;
+};
+
+const styles = StyleSheet.create({
+  containerStyle: { margin: 10 }
+});
+
+export default Container;
