35acd42e5dae3b3c7d8bd7829ab95fca7f446e8f
sheronW <sheronw1174@gmail.com>
2020-01-23 15:53:20 +0800

    layout for artist info

--- src/screens/ArtistScreen.js
+++ src/screens/ArtistScreen.js
@@ -1,18 +1,35 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 const ArtistScreen = ({ navigation }) => {
   const music_id = navigation.getParam("music_id");
   const title = navigation.getParam("title");
-  const results;
+  const result = {};
+  const { images, name } = result;
+
   return (
     <View>
-      <Text style={styles.headerStyle}>ArtistScreen</Text>
+      <Image
+        style={{
+          width: "50%",
+          aspectRatio: images[0].width / images[0].height
+        }}
+        source={{ uri: images[0].url }}
+      />
+      <Text style={styles.title}>{name}</Text>
+      <View style={{ flexDirection: "row" }}>
+        <Text style={styles.subtitle}>
+          Your rating: <Text style={styles.rating}>{rating}</Text>
+        </Text>
+        <Text style={styles.subtitle}>
+          Average rating: <Text style={styles.rating}>{avg_rating}</Text>
+        </Text>
+      </View>
       <FlatList
         data={results}
-        keyExtracter={i => i.item.name}
-        renderItem={i => {
-          return <ListPreview result={i.item} />;
+        keyExtracter={item => item.name}
+        renderItem={({ item }) => {
+          return <ListPreview result={item} />;
         }}
         columnWrapperStyle={styles.column}
         numColumns={2}
@@ -22,10 +39,24 @@ const ArtistScreen = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
-  headerStyle: {
-    fontSize: 60
+  container: {
+    backgroundColor: "#FFF",
+    flexDirection: "column",
+    flex: 1,
+    alignItems: "center"
   },
-  column: { flexShrink: 1, width: "50%" }
+  column: { flexShrink: 1, width: "50%" },
+  title: { fontSize: 30, color: colors.text },
+  subtitle: {
+    fontSize: 18,
+    color: colors.text,
+    padding: 10,
+    paddingBottom: 40
+  },
+  rating: {
+    color: colors.primary,
+    fontWeight: "bold"
+  }
 });
 
 export default ArtistScreen;
