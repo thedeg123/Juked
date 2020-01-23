e56918d1d149c4f2f8a93cff03e0d248bbf9b347
david <thedeg123@gmail.com>
2020-01-23 14:02:44 -0700

    used container with search

--- src/components/SearchStyle.js
+++ src/components/SearchStyle.js
@@ -1,43 +1,40 @@
 import React from "react";
 import { View, StyleSheet, Button } from "react-native";
-
 // import colors
 
 const SearchStyle = ({ searchType, setSearchType, onChangeButton }) => {
   return (
-    <View>
-      <View style={styles.backgroundStyle}>
-        <Button
-          title="Songs"
-          style={styles.buttonStyle}
-          color={searchType === "track" ? "red" : "#3480eb"}
-          onPress={() => {
-            onChangeButton("track");
-          }}
-        />
-        <Button
-          title="Albums"
-          style={styles.buttonStyle}
-          color={searchType === "album" ? "red" : "#3480eb"}
-          onPress={() => {
-            onChangeButton("album");
-          }}
-        />
-        <Button
-          title="Artists"
-          style={styles.buttonStyle}
-          color={searchType === "artist" ? "red" : "#3480eb"}
-          onPress={() => {
-            onChangeButton("artist");
-          }}
-        />
-        <Button
-          title="Users"
-          style={styles.buttonStyle}
-          color={searchType === "user" ? "red" : "#3480eb"}
-          onPress={() => setSearchType("user")}
-        />
-      </View>
+    <View style={styles.backgroundStyle}>
+      <Button
+        title="Songs"
+        style={styles.buttonStyle}
+        color={searchType === "track" ? "red" : "#3480eb"}
+        onPress={() => {
+          onChangeButton("track");
+        }}
+      />
+      <Button
+        title="Albums"
+        style={styles.buttonStyle}
+        color={searchType === "album" ? "red" : "#3480eb"}
+        onPress={() => {
+          onChangeButton("album");
+        }}
+      />
+      <Button
+        title="Artists"
+        style={styles.buttonStyle}
+        color={searchType === "artist" ? "red" : "#3480eb"}
+        onPress={() => {
+          onChangeButton("artist");
+        }}
+      />
+      <Button
+        title="Users"
+        style={styles.buttonStyle}
+        color={searchType === "user" ? "red" : "#3480eb"}
+        onPress={() => setSearchType("user")}
+      />
     </View>
   );
 };
@@ -49,7 +46,6 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     marginVertical: 10,
     justifyContent: "space-between",
-    marginHorizontal: 10,
     borderWidth: 1,
     borderColor: "#3480eb"
   },
