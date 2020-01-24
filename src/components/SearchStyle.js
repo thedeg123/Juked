793b0fb20e472e60b5a5bd187f015234bc168edd
Alex Jensen <ajensen@oberlin.edu>
2020-01-23 19:06:09 -0500

    Updated SearchStyle to look like on HomeScreen

--- src/components/SearchStyle.js
+++ src/components/SearchStyle.js
@@ -1,57 +1,120 @@
-import React from "react";
-import { View, StyleSheet, Button } from "react-native";
-// import colors
+import React, { useState } from "react";
+import { View, StyleSheet, Button, TouchableOpacity, Text } from "react-native";
+import colors from "../constants/colors";
 
-const SearchStyle = ({ searchType, setSearchType, onChangeButton }) => {
+const SearchStyle = ({ options, setSearchType, onChangeButton }) => {
+  const activatedStyle = {
+    background: styles.activatedStyleBackground,
+    text: styles.activatedStyleText
+  };
+  const deactivatedStyle = {
+    background: styles.deactivatedStyleBackground,
+    text: styles.deactivatedStyleText
+  };
+
+  const [style1, setStyle1] = useState(activatedStyle);
+  const [style2, setStyle2] = useState(deactivatedStyle);
+  const [style3, setStyle3] = useState(deactivatedStyle);
+  const [style4, setStyle4] = useState(deactivatedStyle);
+
+  const updateColors = setNum => {
+    if (setNum === 1) {
+      setStyle1(activatedStyle);
+      setStyle2(deactivatedStyle);
+      setStyle3(deactivatedStyle);
+      setStyle4(deactivatedStyle);
+    } else if (setNum === 2) {
+      setStyle1(deactivatedStyle);
+      setStyle2(activatedStyle);
+      setStyle3(deactivatedStyle);
+      setStyle4(deactivatedStyle);
+    } else if (setNum === 3) {
+      setStyle1(deactivatedStyle);
+      setStyle2(deactivatedStyle);
+      setStyle3(activatedStyle);
+      setStyle4(deactivatedStyle);
+    } else {
+      setStyle1(deactivatedStyle);
+      setStyle2(deactivatedStyle);
+      setStyle3(deactivatedStyle);
+      setStyle4(activatedStyle);
+    }
+  };
+
   return (
-    <View style={styles.backgroundStyle}>
-      <Button
-        title="Songs"
-        style={styles.buttonStyle}
-        color={searchType === "track" ? "red" : "#3480eb"}
+    <View style={styles.listStyle}>
+      <TouchableOpacity
+        style={style1.background}
         onPress={() => {
+          updateColors(1);
           onChangeButton("track");
         }}
-      />
-      <Button
-        title="Albums"
-        style={styles.buttonStyle}
-        color={searchType === "album" ? "red" : "#3480eb"}
+      >
+        <Text style={style1.text}>{options[0]}</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={style2.background}
         onPress={() => {
+          updateColors(2);
           onChangeButton("album");
         }}
-      />
-      <Button
-        title="Artists"
-        style={styles.buttonStyle}
-        color={searchType === "artist" ? "red" : "#3480eb"}
+      >
+        <Text style={style2.text}>{options[1]}</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={style3.background}
         onPress={() => {
+          updateColors(3);
           onChangeButton("artist");
         }}
-      />
-      <Button
-        title="Users"
-        style={styles.buttonStyle}
-        color={searchType === "user" ? "red" : "#3480eb"}
-        onPress={() => setSearchType("user")}
-      />
+      >
+        <Text style={style3.text}>{options[2]}</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={style4.background}
+        onPress={() => {
+          updateColors(4);
+          setSearchType("user");
+        }}
+      >
+        <Text style={style4.text}>{options[3]}</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  backgroundStyle: {
+  listStyle: {
+    padding: 2,
+    backgroundColor: colors.primary,
+    borderColor: colors.primary,
+    borderRadius: 3,
     flexDirection: "row",
-    alignItems: "stretch",
-    justifyContent: "center",
-    marginVertical: 10,
-    justifyContent: "space-between",
-    borderWidth: 1,
-    borderColor: "#3480eb"
+    justifyContent: "space-around"
   },
-  buttonStyle: {
+  activatedStyleBackground: {
+    paddingVertical: 2,
     flex: 1,
-    alignSelf: "center"
+    backgroundColor: colors.primary
+  },
+  activatedStyleText: {
+    textAlign: "center",
+    color: colors.background,
+    fontSize: 20
+  },
+  deactivatedStyleBackground: {
+    paddingVertical: 2,
+    flex: 1,
+    backgroundColor: colors.background,
+    color: "blue"
+  },
+  deactivatedStyleText: {
+    color: colors.primary,
+    textAlign: "center",
+    fontSize: 20
   }
 });
 
