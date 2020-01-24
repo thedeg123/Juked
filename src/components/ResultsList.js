53418d66273e2a475ac9aa22a91e217bafe21e4f
Alex Jensen <ajensen@oberlin.edu>
2020-01-24 11:12:15 -0500

    Changed ResultsList to switch statement

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -6,76 +6,80 @@ import SearchPreview from "./SearchPreview";
 const ResultsList = ({ searchType, search, navigation }) => {
   if (search === null) {
     return null;
-  } else if (searchType === "track") {
-    return (
-      <View style={{ flex: 1 }}>
-        <FlatList
-          data={search.tracks.items.slice(0, 20)}
-          keyExtractor={searchItem => searchItem.id}
-          renderItem={({ item }) => {
-            return (
-              <View>
-                <SearchPreview
-                  title={search ? `${item.name}` : "none"}
-                  type={searchType}
-                  music_id={search ? `${item.id}` : "none"}
-                  navigation={navigation}
-                />
-              </View>
-            );
-          }}
-        />
-      </View>
-    );
-  } else if (searchType === "album") {
-    return (
-      <View style={{ flex: 1 }}>
-        <FlatList
-          data={search.albums.items.slice(0, 50)}
-          keyExtractor={searchItem => searchItem.id}
-          renderItem={({ item }) => {
-            return (
-              <View>
-                <SearchPreview
-                  title={search ? `${item.name}` : "none"}
-                  type={searchType}
-                  music_id={search ? `${item.id}` : "none"}
-                  navigation={navigation}
-                />
-              </View>
-            );
-          }}
-        />
-      </View>
-    );
-  } else if (searchType === "artist") {
-    return (
-      <View style={{ flex: 1 }}>
-        <FlatList
-          data={search.artists.items.slice(0, 50)}
-          keyExtractor={searchItem => searchItem.id}
-          renderItem={({ item }) => {
-            return (
-              <View>
-                <SearchPreview
-                  title={search ? `${item.name}` : "none"}
-                  type={searchType}
-                  music_id={search ? `${item.id}` : "none"}
-                  navigation={navigation}
-                />
-              </View>
-            );
-          }}
-        />
-      </View>
-    );
   } else {
-    // In this case, it is just the user
-    return (
-      <View>
-        <Text>User List</Text>
-      </View>
-    );
+    switch (searchType) {
+      case "track":
+        return (
+          <View style={{ flex: 1 }}>
+            <FlatList
+              data={search.tracks.items.slice(0, 20)}
+              keyExtractor={searchItem => searchItem.id}
+              renderItem={({ item }) => {
+                return (
+                  <View>
+                    <SearchPreview
+                      title={search ? `${item.name}` : "none"}
+                      type={searchType}
+                      music_id={search ? `${item.id}` : "none"}
+                      navigation={navigation}
+                    />
+                  </View>
+                );
+              }}
+            />
+          </View>
+        );
+      case "album":
+        return (
+          <View style={{ flex: 1 }}>
+            <FlatList
+              data={search.albums.items.slice(0, 50)}
+              keyExtractor={searchItem => searchItem.id}
+              renderItem={({ item }) => {
+                return (
+                  <View>
+                    <SearchPreview
+                      title={search ? `${item.name}` : "none"}
+                      type={searchType}
+                      music_id={search ? `${item.id}` : "none"}
+                      navigation={navigation}
+                    />
+                  </View>
+                );
+              }}
+            />
+          </View>
+        );
+      case "artist":
+        return (
+          <View style={{ flex: 1 }}>
+            <FlatList
+              data={search.artists.items.slice(0, 50)}
+              keyExtractor={searchItem => searchItem.id}
+              renderItem={({ item }) => {
+                return (
+                  <View>
+                    <SearchPreview
+                      title={search ? `${item.name}` : "none"}
+                      type={searchType}
+                      music_id={search ? `${item.id}` : "none"}
+                      navigation={navigation}
+                    />
+                  </View>
+                );
+              }}
+            />
+          </View>
+        );
+      case "user":
+        return (
+          <View>
+            <Text>User List</Text>
+          </View>
+        );
+      default:
+        return null;
+    }
   }
 };
 
