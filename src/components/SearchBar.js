f21d7dd098a8e8fe62a770e1ad4d2da51b615b1e
david <thedeg123@gmail.com>
2020-03-30 20:11:14 -0600

    added cancel button to search

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,34 +1,56 @@
-import React from "react";
-import { TextInput, View, StyleSheet } from "react-native";
+import React, { useState, useRef } from "react";
+import {
+  TextInput,
+  View,
+  TouchableOpacity,
+  Text,
+  StyleSheet
+} from "react-native";
 import { FontAwesome, AntDesign } from "@expo/vector-icons";
 import colors from "../constants/colors";
 
 const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
+  const [cancel, setCancel] = useState(false);
+  const inputRef = useRef(null);
   return (
-    <View style={styles.backgroundStyle}>
-      <FontAwesome
-        name="search"
-        style={styles.iconStyle}
-        color={colors.primary}
-      />
-      <TextInput
-        style={styles.inputStyle}
-        placeholder="Search"
-        placeholderTextColor={colors.shadow}
-        autoCorrect={false}
-        autoCapitalize="none"
-        returnKeyType={"search"}
-        value={term}
-        onChangeText={onTermChange}
-        onEndEditing={onTermSubmit}
-      />
-      {term !== "" ? (
-        <AntDesign
-          name="close"
+    <View style={styles.wrapper}>
+      <View style={styles.backgroundStyle}>
+        <FontAwesome
+          name="search"
           style={styles.iconStyle}
           color={colors.primary}
-          onPress={() => onTermChange("")}
         />
+        <TextInput
+          ref={inputRef}
+          style={styles.inputStyle}
+          placeholder="Search"
+          placeholderTextColor={colors.shadow}
+          autoCorrect={false}
+          autoCapitalize="none"
+          returnKeyType={"search"}
+          value={term}
+          onChangeText={onTermChange}
+          onEndEditing={onTermSubmit}
+          onFocus={() => setCancel(true)}
+          onSubmitEditing={() => setCancel(false)}
+        />
+        {term !== "" ? (
+          <AntDesign
+            name="close"
+            style={styles.iconStyle}
+            onPress={() => onTermChange("")}
+          />
+        ) : null}
+      </View>
+      {cancel ? (
+        <TouchableOpacity
+          onPress={() => {
+            inputRef.current.blur();
+            return setCancel(false);
+          }}
+        >
+          <Text style={styles.cancelStyle}>Cancel</Text>
+        </TouchableOpacity>
       ) : null}
     </View>
   );
@@ -41,7 +63,18 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     backgroundColor: "#ccc",
     height: 40,
+    flex: 1,
     flexDirection: "row"
+  },
+  wrapper: {
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row"
+  },
+  cancelStyle: {
+    color: colors.primary,
+    fontSize: 18,
+    bottom: 5
   },
   inputStyle: {
     marginHorizontal: 10,
@@ -49,6 +82,7 @@ const styles = StyleSheet.create({
     flex: 1
   },
   iconStyle: {
+    color: colors.primary,
     marginHorizontal: 10,
     fontSize: 25,
     alignSelf: "center"
