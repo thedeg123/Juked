2150ec10864991fc1185ae8a9eaa272fca6ed0c0
david <thedeg123@gmail.com>
2020-04-22 12:39:11 -0600

    updated cloud funcs to keep num of rating + review

--- functions/index.js
+++ functions/index.js
@@ -28,25 +28,28 @@ exports.updateContent = functions.firestore
     const refContent = await ref.get();
     if (refContent.exists) {
       const content = refContent.data();
-      const number_reviews = content.number_reviews + 1;
+      const number_ratings = content.number_ratings + 1;
+      const number_reviews = content.number_reviews + Number(content.is_review);
       const sum_reviews = content.sum_reviews + newReview.rating;
-      const review_nums = content.review_nums;
-      review_nums[Number(newReview.rating)] += 1;
+      const rating_nums = content.rating_nums;
+      rating_nums[Number(newReview.rating)] += 1;
       ref.update({
+        number_ratings,
         number_reviews,
         sum_reviews,
-        avg: sum_reviews / number_reviews,
-        review_nums
+        avg: sum_reviews / number_ratings || 0,
+        rating_nums
       });
     } else {
-      const review_nums = new Array(11).fill(0);
-      review_nums[Number(newReview.rating)] = 1;
+      const rating_nums = new Array(11).fill(0);
+      rating_nums[Number(newReview.rating)] = 1;
       ref.set({
-        number_reviews: 1,
+        number_ratings: 1,
+        number_reviews: newReview.is_review ? 1 : 0,
         content_id: newReview.content_id,
         sum_reviews: newReview.rating,
         avg: newReview.rating,
-        review_nums
+        rating_nums
       });
     }
     //updating the array of the users number of reviews
@@ -66,16 +69,21 @@ exports.updateContentOnEdit = functions.firestore
     const newReview = change.after.data();
     const oldReview = change.before.data();
     const ref = await firestore.collection("content").doc(oldReview.content_id);
-    const refContent = await ref.get().then(res => res.data());
-    const review_nums = refContent.review_nums;
-    review_nums[Number(newReview.rating)] += 1;
-    review_nums[Number(oldReview.rating)] -= 1;
+    const content = await ref.get().then(res => res.data());
+    const rating_nums = content.rating_nums;
+    rating_nums[Number(newReview.rating)] += 1;
+    rating_nums[Number(oldReview.rating)] -= 1;
+    const number_reviews =
+      content.number_reviews -
+      Number(oldReview.is_review) +
+      Number(newReview.is_review);
     const sum_reviews =
-      refContent.sum_reviews + newReview.rating - oldReview.rating;
+      content.sum_reviews + newReview.rating - oldReview.rating;
     ref.update({
       sum_reviews,
-      avg: sum_reviews / refContent.number_reviews,
-      review_nums
+      number_reviews,
+      avg: sum_reviews / content.number_ratings || 0,
+      rating_nums
     });
     //updating the array of the users number of reviews
     const user = await firestore.collection("users").doc(newReview.author);
@@ -94,19 +102,21 @@ exports.updateContentOnDelete = functions.firestore
   .onDelete(async (snap, context) => {
     const delReview = snap.data();
     const ref = await firestore.collection("content").doc(delReview.content_id);
-    const refContent = await ref.get().then(res => res.data());
-    const sum_reviews = refContent.sum_reviews - delReview.rating;
-    const number_reviews = refContent.number_reviews - 1;
-    const review_nums = refContent.review_nums;
-    review_nums[Number(delReview.rating)] -= 1;
+    const content = await ref.get().then(res => res.data());
+    const sum_reviews = content.sum_reviews - delReview.rating;
+    const number_ratings = content.number_ratings - 1;
+    const number_reviews = content.number_reviews - Number(delReview.is_review);
+    const rating_nums = content.rating_nums;
+    rating_nums[Number(delReview.rating)] -= 1;
     ref.update({
       sum_reviews,
+      number_ratings,
       number_reviews,
-      avg: sum_reviews / number_reviews,
-      review_nums
+      avg: sum_reviews / number_ratings || 0,
+      rating_nums
     });
     //updating the array of the users number of reviews
-    const user = await firestore.collection("users").doc(newReview.author);
+    const user = await firestore.collection("users").doc(delReview.author);
     const userContent = await user.get().then(res => res.data());
     //TODO: DELETE OR CASE ON LAUNCH
     const review_data = userContent.review_data || new Array(11).fill(0);
